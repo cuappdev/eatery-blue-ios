94fb3bdc6eeea8921ba8d5e2bf9f5231ceefb6fb
Jennifer <jennifergu.2003@gmail.com>
2023-04-22 13:51:35 -0400

    Add analytics

--- Eatery Blue/Eatery Blue/AppDelegate.swift
+++ Eatery Blue/Eatery Blue/AppDelegate.swift
@@ -20,8 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        // TODO: Uncomment after reedownloading GoogleService-Info.plist
-//        FirebaseApp.configure()
+        FirebaseApp.configure()
         return true
     }
 
