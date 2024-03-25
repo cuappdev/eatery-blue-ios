6dbb8c0268ef2b12853e28f3748b690ebdc05f11
Cassidy Xu <chx4@cornell.edu>
2024-03-25 18:08:42 -0400

    Fix spacing

--- Eatery Blue/UI/EateryScreen/MenuHeaderView.swift
+++ Eatery Blue/UI/EateryScreen/MenuHeaderView.swift
@@ -12,6 +12,7 @@ class MenuHeaderView: UIView {
     let titleLabel = UILabel()
     let subtitleLabel = UILabel()
     let buttonImageView = UIImageView()
+    let menuInaccuracyLabel = UILabel()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -33,6 +34,9 @@ class MenuHeaderView: UIView {
 
         addSubview(buttonImageView)
         setUpButtonImageView()
+
+        addSubview(menuInaccuracyLabel)
+        setUpMenuInaccuracyLabel()
     }
 
     private func setUpTitleLabel() {
@@ -50,6 +54,13 @@ class MenuHeaderView: UIView {
         buttonImageView.isUserInteractionEnabled = true
     }
 
+    private func setUpMenuInaccuracyLabel() {
+        menuInaccuracyLabel.text = "*Menus are based on Cornell Dining and are subject to change"
+        menuInaccuracyLabel.font = .preferredFont(for: .caption2, weight: .semibold)
+        menuInaccuracyLabel.textColor = UIColor.Eatery.gray05
+    }
+
+
     private func setUpConstraints() {
         titleLabel.snp.makeConstraints { make in
             make.top.leading.equalTo(layoutMarginsGuide)
@@ -57,14 +68,20 @@ class MenuHeaderView: UIView {
 
         subtitleLabel.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).offset(4)
-            make.leading.bottom.equalTo(layoutMarginsGuide)
+            make.leading.equalTo(layoutMarginsGuide)
         }
 
         buttonImageView.snp.makeConstraints { make in
             make.leading.equalTo(titleLabel.snp.trailing).offset(8)
             make.leading.equalTo(subtitleLabel.snp.trailing).offset(8)
             make.width.height.equalTo(40)
-            make.trailing.centerY.equalTo(layoutMarginsGuide)
+            make.trailing.equalTo(layoutMarginsGuide)
+            make.centerY.equalTo(layoutMarginsGuide).offset(-8)
+        }
+
+        menuInaccuracyLabel.snp.makeConstraints { make in
+            make.top.equalTo(subtitleLabel.snp.bottom).offset(12)
+            make.leading.bottom.equalTo(layoutMarginsGuide)
         }
     }
 
