77146ff21f35662f0373a307d43d593eec6f68b1
Peter <pjb294@cornell.edu>
2024-09-11 16:30:12 -0400

    address pr comments

--- Eatery Blue/UI/Util/ScrollHitView.swift
+++ Eatery Blue/UI/Util/ScrollHitView.swift
@@ -27,22 +27,22 @@ class ScrollHitView: UIView {
     // MARK: - Setup
 
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        
+        var answer = scrollView as UIView // if nothing is found we will do the expected
+
         // find the UIStackView inside, if there is none then we just return the scrollView
-        guard let stackView = scrollView.subviews.first(where: { subview in
-            return subview is UIStackView
-        }) else { return scrollView }
+        guard let stackView = scrollView.subviews.first(where: { $0 is UIStackView }) else { return answer }
 
         let adjustedPoint = point.applying(CGAffineTransform(translationX: scrollView.contentOffset.x - (self.frame.width / 2 - scrollView.frame.width / 2), y: 0))
         // Go through the stackView's subviews. If one is within hit point, return that
-        for stackSubView in stackView.subviews {
-            if stackSubView.frame.contains(adjustedPoint) {
-                print("hit", stackSubView)
-                return stackSubView
+        stackView.subviews.forEach {
+            if $0.frame.contains(adjustedPoint) {
+                answer = $0
             }
         }
 
-        // otherwise return the scrollView
-        return scrollView
+        // return the answer
+        return answer
     }
 
 }
