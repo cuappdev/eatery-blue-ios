4034f1cfd3203237ca35b1d2457f54a58ba4bacb
Peter <mrpeterssyt@gmail.com>
2025-04-09 20:48:32 -0400

    allergen + dietary preference support

--- Eatery Blue/UI/EateryScreen/MenuItemView.swift
+++ Eatery Blue/UI/EateryScreen/MenuItemView.swift
@@ -12,7 +12,9 @@ class MenuItemView: UIView {
 
     // MARK: - Properties (view)
 
+    private let allergensLabel = UILabel()
     private let descriptionLabel = UILabel()
+    private let dietaryInfoStack = UIStackView()
     private let favoriteButton = ButtonView(content: UIView())
     private let favoriteButtonImage = UIImageView()
     private let priceLabel = UILabel()
@@ -28,6 +30,8 @@ class MenuItemView: UIView {
             setUpPriceLabel()
             setUpDescriptionLabel()
             setUpFavoriteButton()
+            setUpAllergensLabel(item?.allergens ?? [])
+            setUpDietaryInfoStack(item?.dietaryPreferences ?? [])
         }
     }
 
@@ -47,7 +51,7 @@ class MenuItemView: UIView {
     private func setUpSelf() {
         layoutMargins = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
         insetsLayoutMarginsFromSafeArea = false
-        
+
         addSubview(stackView)
         setUpStackView()
     }
@@ -68,6 +72,9 @@ class MenuItemView: UIView {
         titlePriceContainer.addSubview(titleLabel)
         setUpTitleLabel()
 
+        titlePriceContainer.addSubview(dietaryInfoStack)
+        setUpDietaryInfoStack([])
+
         titlePriceContainer.addSubview(priceLabel)
         setUpPriceLabel()
 
@@ -77,6 +84,9 @@ class MenuItemView: UIView {
 
         stackView.addArrangedSubview(descriptionLabel)
         setUpDescriptionLabel()
+
+        stackView.addArrangedSubview(allergensLabel)
+        setUpAllergensLabel([])
     }
 
     private func setUpTitleLabel() {
@@ -132,14 +142,67 @@ class MenuItemView: UIView {
         }
     }
 
+    private func setUpAllergensLabel(_ allergens: [String]) {
+        if allergens.isEmpty {
+            allergensLabel.isHidden = true
+            return
+        } else {
+            allergensLabel.isHidden = false
+        }
+
+        allergensLabel.text = "Contains: \(allergens.joined(separator: ", "))"
+        allergensLabel.font = .systemFont(ofSize: 10)
+        allergensLabel.textColor = UIColor.Eatery.red
+    }
+
+    private func setUpDietaryInfoStack(_ dietaryInfo: [String]) {
+        dietaryInfoStack.axis = .horizontal
+        dietaryInfoStack.spacing = 8
+
+        if dietaryInfo.isEmpty {
+            dietaryInfoStack.isHidden = true
+            return
+        } else {
+            dietaryInfoStack.isHidden = false
+        }
+
+        dietaryInfoStack.subviews.forEach { view in
+            dietaryInfoStack.removeArrangedSubview(view)
+            view.removeFromSuperview()
+        }
+
+        var newDietaryInfo = dietaryInfo
+        newDietaryInfo.append("Halal")
+        newDietaryInfo.append("Nut-free")
+        newDietaryInfo.append("Kosher")
+
+        newDietaryInfo.forEach { dietaryInfo in
+            let dietaryInfoView = UIImageView()
+            // see if the dietaryInfo image exists
+            if let image = UIImage(named: dietaryInfo) {
+                dietaryInfoView.image = image
+                dietaryInfoStack.addArrangedSubview(dietaryInfoView)
+                dietaryInfoView.snp.makeConstraints { make in
+                    make.size.equalTo(22)
+                }
+            }
+        }
+
+
+    }
+
     private func setUpConstraints() {
         titleLabel.snp.makeConstraints { make in
             make.top.leading.bottom.equalToSuperview()
         }
 
+        dietaryInfoStack.snp.makeConstraints { make in
+            make.leading.equalTo(titleLabel.snp.trailing).offset(4)
+            make.centerY.equalTo(titleLabel)
+        }
+
         favoriteButton.snp.makeConstraints { make in
-            make.leading.equalTo(priceLabel)
-            make.top.bottom.trailing.equalToSuperview()
+            make.trailing.centerY.equalToSuperview()
             make.size.equalTo(24)
         }
 
@@ -148,9 +211,9 @@ class MenuItemView: UIView {
         }
 
         priceLabel.snp.makeConstraints { make in
-            make.leading.equalTo(titleLabel.snp.trailing)
-            make.trailing.equalTo(favoriteButton)
-            make.top.bottom.equalToSuperview()
+            make.leading.equalTo(dietaryInfoStack.snp.trailing).offset(8)
+            make.trailing.equalTo(favoriteButton.snp.leading).offset(-4)
+            make.centerY.equalToSuperview()
         }
 
         priceLabel.setContentHuggingPriority(
@@ -164,6 +227,10 @@ class MenuItemView: UIView {
 
         stackView.snp.makeConstraints { make in
             make.edges.equalTo(layoutMarginsGuide)
+        }
+
+        allergensLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
         }
     }
 
