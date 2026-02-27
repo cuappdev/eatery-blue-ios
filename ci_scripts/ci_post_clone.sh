2bcb62c48e6435c0caf3dcdea795a07849269104
Peter <pjb294@cornell.edu>
2026-02-27 11:43:14 -0500

    Update ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,12 +7,11 @@
 #
 
 # Install Minio Client
-curl https://dl.min.io/client/mc/release/darwin-amd64/mc -o mc
-chmod +x mc
+brew install minio/stable/mc
 
 # Sync secrets from DigitalOcean Spaces
-./mc alias set my-space https://nyc3.digitaloceanspaces.com "$SPACES_ACCESS_KEY_ID" "$SPACES_SECRET_ACCESS_KEY"
-./mc mirror my-space/appdev-upload/ios-secrets/eatery-blue/ "$CI_PRIMARY_REPOSITORY_PATH/Eatery Blue/Supporting"
+mc alias set my-space https://nyc3.digitaloceanspaces.com "$SPACES_ACCESS_KEY_ID" "$SPACES_SECRET_ACCESS_KEY"
+mc mirror my-space/appdev-upload/ios-secrets/eatery-blue/ "$CI_PRIMARY_REPOSITORY_PATH/Eatery Blue/Supporting"
 
 # Trust swiftlint
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
