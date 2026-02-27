d4db488ca712e691321a33f1b3580392de0a44df
Peter <pjb294@cornell.edu>
2026-02-27 11:35:41 -0500

    Update ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,16 +6,13 @@
 #  Created by Vin Bui on 10/9/23.
 #
 
-brew install awscli
+# Install Minio Client
+curl https://dl.min.io/client/mc/release/darwin-amd64/mc -o mc
+chmod +x mc
 
-export AWS_ACCESS_KEY_ID="$SPACES_ACCESS_KEY_ID"
-export AWS_SECRET_ACCESS_KEY="$SPACES_SECRET_ACCESS_KEY"
-
-aws s3 sync \
-    s3://appdev-upload/ios-secrets/eatery-blue/ \
-    "$CI_WORKSPACE/Eatery Blue/Supporting" \
-    --endpoint-url=https://nyc3.digitaloceanspaces.com \
-    --no-progress 2>&1 | grep -v "Is a directory"
+# Sync secrets from DigitalOcean Spaces
+./mc alias set my-space https://nyc3.digitaloceanspaces.com "$SPACES_ACCESS_KEY_ID" "$SPACES_SECRET_ACCESS_KEY"
+./mc mirror my-space/appdev-upload/ios-secrets/eatery-blue/ "$CI_PRIMARY_REPOSITORY_PATH/Eatery Blue/Supporting"
 
 # Trust swiftlint
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
