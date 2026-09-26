2ccf27d5827a25371ccdb1b7f4428df993311d7c
ArielleNu <ariellenudelman2@gmail.com>
2026-09-26 11:54:01 -0400

    Revert "Fix favorites search navigation"
    This reverts commit 4870c018144a9f87b980084685f5217365440359.

--- Eatery Blue/UI/Favoriting/FavoritesNavigationView.swift
+++ Eatery Blue/UI/Favoriting/FavoritesNavigationView.swift
@@ -10,12 +10,12 @@ import UIKit
 class FavoritesNavigationView: UIView {
     // MARK: - Properties (view)
 
-    private let backButton = UIButton(type: .system)
+    private let backButton = ButtonView(content: UIImageView())
     private let eateriesTab = TabButtonView()
     private let itemsTab = TabButtonView()
     private let placeholderView = UIView()
     private let searchBar = UISearchBar()
-    private let searchButton = UIButton(type: .system)
+    private let searchButton = ButtonView(content: UIImageView())
     private let titleLabel = UILabel()
 
     // MARK: - Properties (data)
@@ -32,32 +32,27 @@ class FavoritesNavigationView: UIView {
     /// Whether or not the search bar should be shown
     var searchShown = false {
         didSet {
-            guard searchShown != oldValue else { return }
-
-            if searchShown {
-                searchBar.isHidden = false
-                searchBar.setShowsCancelButton(true, animated: false)
-            } else {
+            if !searchShown {
                 searchBar.text = ""
                 searchDelegate?.searchBar?(searchBar, textDidChange: "")
-                searchBar.resignFirstResponder()
-            }
 
-            let shown = searchShown
-            UIView.animate(withDuration: 0.2, animations: { [weak self] in
-                guard let self else { return }
-                searchBar.alpha = shown ? 1 : 0
-                titleLabel.alpha = shown ? 0 : 1
-            }, completion: { [weak self] _ in
-                guard let self, searchShown == shown else { return }
-                searchBar.isHidden = !shown
-                if !shown {
-                    searchBar.setShowsCancelButton(false, animated: false)
+                UIView.animate(withDuration: 0.1) { [weak self] in
+                    guard let self else { return }
+                    searchBar.snp.updateConstraints { make in
+                        make.height.equalTo(0)
+                    }
+
+                    layoutIfNeeded()
                 }
-            })
+            } else {
+                UIView.animate(withDuration: 0.1) { [weak self] in
+                    guard let self else { return }
+                    searchBar.snp.updateConstraints { make in
+                        make.height.equalTo(36)
+                    }
 
-            if searchShown {
-                searchBar.becomeFirstResponder()
+                    layoutIfNeeded()
+                }
             }
         }
     }
@@ -109,19 +104,21 @@ class FavoritesNavigationView: UIView {
         setUpSearchBar()
 
         setUpConstraints()
-
-        bringSubviewToFront(backButton)
-        bringSubviewToFront(searchButton)
     }
 
     private func setUpBackButton() {
-        configureNavigationButton(backButton, imageNamed: "ArrowLeft")
-        backButton.addAction(UIAction { [weak self] _ in
+        backButton.content.image = UIImage(named: "ArrowLeft")
+        backButton.shadowColor = UIColor.Eatery.primaryText
+        backButton.shadowOffset = CGSize(width: 0, height: 4)
+        backButton.backgroundColor = UIColor.Eatery.default00
+        backButton.layoutMargins = UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 16)
+
+        backButton.buttonPress { [weak self] _ in
             guard let self else { return }
 
             navigationController?.hero.isEnabled = false
             navigationController?.popViewController(animated: true)
-        }, for: .touchUpInside)
+        }
     }
 
     private func setUpTitleLabel() {
@@ -131,29 +128,18 @@ class FavoritesNavigationView: UIView {
     }
 
     private func setUpSearchButton() {
-        configureNavigationButton(searchButton, imageNamed: "Search")
-        searchButton.addAction(UIAction { [weak self] _ in
+        searchButton.content.image = UIImage(named: "Search")
+        searchButton.shadowColor = UIColor.Eatery.primaryText
+        searchButton.shadowOffset = CGSize(width: 0, height: 4)
+        searchButton.backgroundColor = UIColor.Eatery.default00
+        searchButton.layoutMargins = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 0)
+
+        searchButton.buttonPress { [weak self] _ in
             guard let self else { return }
 
             searchShown = true
             searchBar.becomeFirstResponder()
-        }, for: .touchUpInside)
-    }
-
-    private func configureNavigationButton(_ button: UIButton, imageNamed imageName: String) {
-        var configuration: UIButton.Configuration
-        if #available(iOS 26.0, *) {
-            configuration = .glass()
-        } else {
-            configuration = .filled()
-            configuration.baseBackgroundColor = UIColor.Eatery.default01
         }
-
-        configuration.image = UIImage(named: imageName)
-        configuration.baseForegroundColor = UIColor.Eatery.primaryText
-        configuration.cornerStyle = .capsule
-        configuration.contentInsets = .zero
-        button.configuration = configuration
     }
 
     private func setUpEateriesTab() {
@@ -177,16 +163,11 @@ class FavoritesNavigationView: UIView {
     }
 
     private func setUpSearchBar() {
-        searchBar.setShowsCancelButton(false, animated: false)
+        searchBar.setShowsCancelButton(true, animated: false)
         searchBar.placeholder = "Search for faves..."
-        searchBar.searchBarStyle = .minimal
         searchBar.layoutMargins = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
         searchBar.backgroundImage = UIImage()
-        searchBar.backgroundColor = .clear
-        searchBar.alpha = 0
-        searchBar.isHidden = true
-        searchBar.setContentHuggingPriority(.fittingSizeLevel, for: .vertical)
-        searchBar.setContentCompressionResistancePriority(.fittingSizeLevel, for: .vertical)
+        searchBar.backgroundColor = UIColor.Eatery.default00
     }
 
     private func setUpConstraints() {
@@ -229,18 +210,16 @@ class FavoritesNavigationView: UIView {
         }
 
         searchBar.snp.makeConstraints { make in
+            make.top.equalTo(layoutMarginsGuide)
             make.leading.trailing.equalToSuperview()
-            make.centerY.equalTo(titleLabel)
-            make.height.equalTo(56)
+            make.height.equalTo(0)
         }
     }
 }
 
 extension FavoritesNavigationView: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if !scrollView.isDragging {
-            return
-        }
+        if !scrollView.isDragging { return }
 
         if scrollView.contentOffset.x > scrollView.contentSize.width / 4 {
             eateriesTab.selected = false
